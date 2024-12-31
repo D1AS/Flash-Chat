35d369b12072dd9edef8ff8c8957f0255349398b
geogit <g_potakis@hotmail.com>
2024-12-31 17:03:42 -0500

    Add custom cell table view to display messages

--- Flash Chat/Controllers/ChatViewController.swift
+++ Flash Chat/Controllers/ChatViewController.swift
@@ -6,10 +6,22 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hey!"),
+        Message(sender: "a@b.com", body: "Hello!"),
+        Message(sender: "1@2.com", body: "What's up?")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "⚡️FlashChat"
+        
+        
+//        tableView.delegate = self
+        tableView.dataSource =  self
+        title = K.appName
         navigationItem.hidesBackButton = true
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -17,7 +29,6 @@ class ChatViewController: UIViewController {
     
 
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
-        let firebaseAuth = Auth.auth()
         do {
           try Auth.auth().signOut()
             navigationController?.popToRootViewController(animated: true)
@@ -27,3 +38,23 @@ class ChatViewController: UIViewController {
         }
     }
 }
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let  cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+                as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        return cell
+    }
+
+}
+
+//extension ChatViewController: UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(indexPath.row)
+//    }
+//}
--- Flash Chat/Controllers/RegisterViewController.swift
+++ Flash Chat/Controllers/RegisterViewController.swift
@@ -37,7 +37,7 @@ class RegisterViewController: UIViewController {
 //                       }
                     
                     
-                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                    self.performSegue(withIdentifier: K.registerSegue, sender: self)
                 }
             }
         }
